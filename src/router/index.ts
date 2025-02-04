71118e6031b2e4a69fdc2d3701801099cf5561a6
Abhay2133 <abhaybishthestudent@gmail.com>
2025-02-04 23:10:49 +0530

    build lint fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createMemoryHistory, createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
 import NewView from "../views/NewView.vue";
 
 const routes = [
   { path: "/", component: HomeView },
-  { path: "/new", component: ()=>import("../views/NewView.vue") },
+  { path: "/new", component: NewView },
 ];
 
 const router = createRouter({
